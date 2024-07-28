b5e71e7116108d27403a5c6275a6338d2f97dc71
Michael Snook <michael.snook@gmail.com>
2024-07-29 01:45:18 +0530

    Make the header padding/font suuuper responsive
    Having so much fun with clamp utilities for font size and paddings!

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -10,15 +10,13 @@ export default function Header() {
   })
   return (
     <>
-      <span ref={ref} className="bg-beige m-0 h-4 p-0">
+      <span ref={ref} className="bg-beige m-0 flex h-4 w-full p-0">
         &nbsp;
       </span>
       <header
-        className={`bg-beige sticky left-0 right-0 top-0 z-50 ${inView === false ? 'shadow-lg' : ''}`}
+        className={`${inView === false ? 'shadow-lg' : ''} bg-beige sticky left-0 right-0 top-0 z-50 mb-[clamp(6px,1.5vw,1rem)] pb-[clamp(6px,1.5vw,1rem)] pt-[clamp(6px,1.5vw,1rem)]`}
       >
-        <h1
-          className={`mx-auto bg-gradient-to-r from-red-500 to-red-900 bg-clip-text ${inView ? 'pb-8' : 'mb-4 pb-4'} pt-4 text-center text-6xl font-extrabold uppercase text-transparent`}
-        >
+        <h1 className="mx-auto max-w-[660px] bg-gradient-to-r from-red-500 to-red-900 bg-clip-text text-center text-[min(9vw,4rem)] font-extrabold uppercase text-transparent">
           No Genocide Josh
         </h1>
       </header>
